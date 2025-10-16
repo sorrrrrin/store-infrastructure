59766dcd58b2bf3048ff3257938c812700e84d5d
sorrrrrin <46755684+sorrrrrin@users.noreply.github.com>
2025-10-16 12:40:53 +0300

    - added github actions CI/CD pipeline (CD)

--- startup.bat
+++ startup.bat
@@ -2,15 +2,19 @@
 --start cmd /k "echo minikube stop && minikube stop"
 --timeout /t 30 /nobreak
 start cmd /k "echo minikube start  --memory=6g && minikube start  --memory=6g"
-timeout /t 45 /nobreak
+timeout /t 60 /nobreak
 
 start cmd /k "echo minikube dashboard && minikube dashboard"
+start cmd /k "echo kubectl port-forward svc/postgresql 5432:5432 -n backend && kubectl port-forward svc/postgresql 5432:5432 -n backend"
 start cmd /k "echo kubectl port-forward -n monitoring svc/prometheus-service 9090:9090 && kubectl port-forward -n monitoring svc/prometheus-service 9090:9090"
 start cmd /k "echo kubectl port-forward -n monitoring svc/grafana 3000:80 && kubectl port-forward -n monitoring svc/grafana 3000:80"
-start cmd /k "echo kubectl port-forward svc/catalog-service -n backend 8181:8080 && kubectl port-forward svc/catalog-service -n backend 8181:8080"
-start cmd /k "echo kubectl port-forward svc/postgresql 5432:5432 -n backend && kubectl port-forward svc/postgresql 5432:5432 -n backend"
 start cmd /k "echo kubectl port-forward svc/elasticsearch-master -n monitoring 9200:9200 && kubectl port-forward svc/elasticsearch-master -n monitoring 9200:9200	"
 start cmd /k "echo kubectl port-forward svc/elk-kibana-kibana -n monitoring 5601:5601 && kubectl port-forward svc/elk-kibana-kibana -n monitoring 5601:5601"
+start cmd /k "echo kubectl port-forward svc/catalog-service -n backend 8180:8080 && kubectl port-forward svc/catalog-service -n backend 8180:8080"
+start cmd /k "echo kubectl port-forward svc/order-service -n dev 8181:8081 && kubectl port-forward svc/order-service -n dev 8181:8081"
+start cmd /k "echo kubectl port-forward svc/user-service -n dev 8182:8082 && kubectl port-forward svc/user-service -n dev 8182:8082"
+
+
 start cmd /k "echo minikube tunnel && minikube tunnel"
 timeout /t 10 /nobreak
 
